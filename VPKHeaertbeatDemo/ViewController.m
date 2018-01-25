1381021cd3a68fd36b9a2bd6f06046d1999e6a75
foundry <foundry.tv@gmail.com>
2018-01-25 01:50:56 +0000

    comments

--- VPKHeaertbeatDemo/ViewController.m
+++ VPKHeaertbeatDemo/ViewController.m
@@ -23,8 +23,6 @@
 @implementation ViewController
 
 
-
-
 #pragma mark - viewController lifecycle
 
 - (void)viewDidLoad {
@@ -37,61 +35,36 @@
 
 }
 
-
-
 #pragma mark - configuration
 
 - (void)configureViewer{
     
     UIImage* image = [UIImage imageNamed:@"tomcruise"];
     NSURL* contentURL = [NSURL URLWithString:@"youtube://ITjsb22-EwQ"];
-    image = [[VPKImage alloc] initWithImage:image url:contentURL];
+    VPKImage* vpkImage = [[VPKImage alloc] initWithImage:image url:contentURL];
+    vpkImage.mediaIdentifier = @"ITjsb22-EwQ";
     self.viewerPreview.image = image;
     
     self.mediaObject =
     [ADBMediaHeartbeat createMediaObjectWithName:@"tomcruise"
-                                         mediaId:@"youtube://ITjsb22-EwQ"
-                                          length:1200
-                                      streamType:@"FILE"];
+                                         mediaId:@"ITjsb22-EwQ"
+                                          length:15
+                                      streamType:ADBMediaHeartbeatStreamTypeLINEAR];
 }
 
-/*
 
 
-- (void)configureViewer2WithTestVideo {
-    
-    UIImage* image = [UIImage imageNamed:@"tomcruise"];
-    NSURL* contentURL = [NSURL URLWithString:@"https://phieagles.akamaized.net//PHI/videos/dct/video_audio/2017/12-December/171204_Westbrook_Review-500k.mp4"];
-    
-    image = [[VPKImage alloc] initWithImage:image url:contentURL];
-    self.viewerPreview2.image = image;
-    self.mediaObject1 =
-    [ADBMediaHeartbeat createMediaObjectWithName:@"tomcruise"
-                                         mediaId:@"youtube://ITjsb22-EwQ"
-                                          length:1200
-                                      streamType:@"FILE"];
-    
-    
-}
-
-- (void)configureViewer2 {
-    UIImage* image = [UIImage imageNamed:@"stik_thin"];
-    image = [[VPKImage alloc] initWithImage:image veepId:@"1787"];
-    self.viewerPreview2.image = image;
-}
-*/
-#pragma mark - example pass-through delegate. Relays UITapGestureRecongizer response in the event a Veep is not available.
+#pragma mark - example pass-through delegate methods.
 
 - (void)vpkPreview:(VPKPreview *)preview passedThroughTap:(UITapGestureRecognizer *)tapGestureRecognizer {
     NSLog(@"tapped %@",tapGestureRecognizer);
 }
 
 - (void)vpkPreview:(VPKPreview *)preview handledTap:(UITapGestureRecognizer *)tapGestureRecognizer {
-    ADBMediaObject* mediaObject = nil;
- 
-    [VPKHeartbeatProvider createSession:preview mediaObject:mediaObject];
-
-
+    /*
+     We could have created the session in `configureViewer:` - which would work in this simple case. However we need to be sure that the analytics data mediaId is associated with the correct media currently playing. In a UITableView or UICollectionView scenario with multiple and reusable VPKPreviews this delegate method is more robust, as it is called only when the VPKVideoPlayer is about to be invoked.
+     */
+    [VPKHeartbeatProvider createSession:preview mediaObject:self.mediaObject];
 }
 
 
