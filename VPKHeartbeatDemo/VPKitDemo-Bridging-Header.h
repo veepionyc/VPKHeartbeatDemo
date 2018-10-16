5580fb070432a3a39fab5755a785247bdd8e7eb2
foundry <foundry.tv@gmail.com>
2018-10-16 20:54:16 +0100

    Swift ViewController subclass to test VPKPreview casting

--- VPKHeartbeatDemo/VPKitDemo-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  VPKitDemo-Bridging-Header.m
-//  VPKitDemo
-//
-//  Created by jonathan on 20/11/2017.
-//  Copyright © 2017 jonathan. All rights reserved.
-//
-
-
---
+++ VPKHeartbeatDemo/ViewControllerSwift.swift
@@ -0,0 +1,27 @@
+//
+//  ViewControllerSwift.swift
+//  VPKHeartbeatDemo
+//
+//  Created by jonathan on 15/10/2018.
+//  Copyright © 2018 jonathan. All rights reserved.
+//
+    
+import UIKit
+
+@objc class ViewControllerSwift: ViewController ,VPKPreviewPassThroughDelegate {
+    
+    
+    
+    
+     func vpkPreview(_ preview:VPKPreview, handledTap tapGestureRecognizer: UITapGestureRecognizer) {
+        var sampleMetadata =  [String:String]()
+         sampleMetadata ["TestMetadaKey"] = "TestMetadaValue"
+        VPKHeartbeatProvider.createSession(
+            preview,
+            mediaObject: self.mediaObject,
+            metadata: sampleMetadata
+        )
+        
+    }
+    
+}
